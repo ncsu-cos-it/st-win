11db414ddf1220edc4fb31dcf2d99ce74587bb12
tbyronnc <terry.byron@gmail.com>
2024-12-06 11:49:02 -0500

    Updated

--- bin/grant_access-wvd.cmd
+++ bin/grant_access-wvd.cmd
@@ -24,24 +24,39 @@ md \\dc2isi00smb.oit.ncsu.edu\cos\stat\Redirect\%username%\Videos
 net use s: \\dc2isi00smb.oit.ncsu.edu\cos\stat\Redirect\%username% /persistent:no
 
 :PERMS
-echo "Documents"
+echo "Modifying Perms"
 if exist "\\dc2isi00smb.oit.ncsu.edu\cos\stat\Redirect\%username%\Documents\.stat" GOTO :END
 Icacls  \\dc2isi00smb.oit.ncsu.edu\cos\stat\Redirect\%username% /grant:r "WOLFTECH\COS-OU Admins":(OI)(CI)F /T /C /L /Q
+echo "Contacts"
 Icacls  \\dc2isi00smb.oit.ncsu.edu\cos\stat\Redirect\%username%\Contacts /grant:r "WOLFTECH\COS-OU Admins":(OI)(CI)F /T /C /L /Q
+echo "Desktop"
 Icacls  \\dc2isi00smb.oit.ncsu.edu\cos\stat\Redirect\%username%\Desktop /grant:r "WOLFTECH\COS-OU Admins":(OI)(CI)F /T /C /L /Q
+echo "Documents"
 Icacls  \\dc2isi00smb.oit.ncsu.edu\cos\stat\Redirect\%username%\Documents /grant:r "WOLFTECH\COS-OU Admins":(OI)(CI)F /T /C /L /Q
+echo "Downloads"
 Icacls  \\dc2isi00smb.oit.ncsu.edu\cos\stat\Redirect\%username%\Downloads /grant:r "WOLFTECH\COS-OU Admins":(OI)(CI)F /T /C /L /Q
+echo "Favorites"
 Icacls  \\dc2isi00smb.oit.ncsu.edu\cos\stat\Redirect\%username%\Favorites /grant:r "WOLFTECH\COS-OU Admins":(OI)(CI)F /T /C /L /Q
+echo "Links"
 Icacls  \\dc2isi00smb.oit.ncsu.edu\cos\stat\Redirect\%username%\Links /grant:r "WOLFTECH\COS-OU Admins":(OI)(CI)F /T /C /L /Q
+echo "Music"
 Icacls  \\dc2isi00smb.oit.ncsu.edu\cos\stat\Redirect\%username%\Music /grant:r "WOLFTECH\COS-OU Admins":(OI)(CI)F /T /C /L /Q
+echo "Pictures"
 Icacls  \\dc2isi00smb.oit.ncsu.edu\cos\stat\Redirect\%username%\Pictures /grant:r "WOLFTECH\COS-OU Admins":(OI)(CI)F /T /C /L /Q
+echo "Saved Games"
 Icacls  "\\dc2isi00smb.oit.ncsu.edu\cos\stat\Redirect\%username%\Saved Games" /grant:r "WOLFTECH\COS-OU Admins":(OI)(CI)F /T /C /L /Q
+echo "Searches"
 Icacls  \\dc2isi00smb.oit.ncsu.edu\cos\stat\Redirect\%username%\Searches /grant:r "WOLFTECH\COS-OU Admins":(OI)(CI)F /T /C /L /Q
+echo "Start Menu"
 Icacls  "\\dc2isi00smb.oit.ncsu.edu\cos\stat\Redirect\%username%\Start Menu" /grant:r "WOLFTECH\COS-OU Admins":(OI)(CI)F /T /C /L /Q
+echo "Videos"
 Icacls  \\dc2isi00smb.oit.ncsu.edu\cos\stat\Redirect\%username%\Videos /grant:r "WOLFTECH\COS-OU Admins":(OI)(CI)F /T /C /L /Q
+echo "AppData"
 Icacls \\dc2isi00smb.oit.ncsu.edu\cos\stat\Redirect\%username%\AppData /grant "WOLFTECH\COS-OU Admins":(OI)(CI)F /T /C /L /Q
+echo "Documents\www"
 md s:\Documents\www
 Icacls \\dc2isi00smb.oit.ncsu.edu\cos\stat\Redirect\%username%\Documents\www /grant:r "WOLFTECH\STAT.web.service":(OI)(CI)RX /T /C /L /Q
+echo "....[Finishing]"
 
 set datestr=%date%
 set result=%datestr:/=-%
--- bin/grant_access.cmd
+++ bin/grant_access.cmd
@@ -6,22 +6,36 @@ if not "%USERDOMAIN%"=="WOLFTECH" GOTO :END
 
 if not exist s:\ GOTO :SDRIVE
 
-echo "Documents"
+echo "Modifying Perms"
 if exist "s:\Documents\.stat" GOTO :PROFILE
 Icacls  \\dc2isi00smb.oit.ncsu.edu\cos\stat\Redirect\%username% /grant:r "WOLFTECH\COS-OU Admins":(OI)(CI)F /T /C /L /Q
+echo "Contacts"
 Icacls  \\dc2isi00smb.oit.ncsu.edu\cos\stat\Redirect\%username%\Contacts /grant:r "WOLFTECH\COS-OU Admins":(OI)(CI)F /T /C /L /Q
+echo "Desktop"
 Icacls  \\dc2isi00smb.oit.ncsu.edu\cos\stat\Redirect\%username%\Desktop /grant:r "WOLFTECH\COS-OU Admins":(OI)(CI)F /T /C /L /Q
+echo "Documents"
 Icacls  \\dc2isi00smb.oit.ncsu.edu\cos\stat\Redirect\%username%\Documents /grant:r "WOLFTECH\COS-OU Admins":(OI)(CI)F /T /C /L /Q
+echo "Downloads"
 Icacls  \\dc2isi00smb.oit.ncsu.edu\cos\stat\Redirect\%username%\Downloads /grant:r "WOLFTECH\COS-OU Admins":(OI)(CI)F /T /C /L /Q
+echo "Favorites"
 Icacls  \\dc2isi00smb.oit.ncsu.edu\cos\stat\Redirect\%username%\Favorites /grant:r "WOLFTECH\COS-OU Admins":(OI)(CI)F /T /C /L /Q
+echo "Links"
 Icacls  \\dc2isi00smb.oit.ncsu.edu\cos\stat\Redirect\%username%\Links /grant:r "WOLFTECH\COS-OU Admins":(OI)(CI)F /T /C /L /Q
+echo "Music"
 Icacls  \\dc2isi00smb.oit.ncsu.edu\cos\stat\Redirect\%username%\Music /grant:r "WOLFTECH\COS-OU Admins":(OI)(CI)F /T /C /L /Q
+echo "Pictures"
 Icacls  \\dc2isi00smb.oit.ncsu.edu\cos\stat\Redirect\%username%\Pictures /grant:r "WOLFTECH\COS-OU Admins":(OI)(CI)F /T /C /L /Q
+echo "Saved Games"
 Icacls  "\\dc2isi00smb.oit.ncsu.edu\cos\stat\Redirect\%username%\Saved Games" /grant:r "WOLFTECH\COS-OU Admins":(OI)(CI)F /T /C /L /Q
+echo "Searches"
 Icacls  \\dc2isi00smb.oit.ncsu.edu\cos\stat\Redirect\%username%\Searches /grant:r "WOLFTECH\COS-OU Admins":(OI)(CI)F /T /C /L /Q
+echo "Start Menu"
 Icacls  "\\dc2isi00smb.oit.ncsu.edu\cos\stat\Redirect\%username%\Start Menu" /grant:r "WOLFTECH\COS-OU Admins":(OI)(CI)F /T /C /L /Q
+echo "Videos"
 Icacls  \\dc2isi00smb.oit.ncsu.edu\cos\stat\Redirect\%username%\Videos /grant:r "WOLFTECH\COS-OU Admins":(OI)(CI)F /T /C /L /Q
+echo "AppData"
 Icacls \\dc2isi00smb.oit.ncsu.edu\cos\stat\Redirect\%username%\AppData /grant "WOLFTECH\COS-OU Admins":(OI)(CI)F /T /C /L /Q
+echo "Documents\www"
 md s:\Documents\www
 Icacls \\dc2isi00smb.oit.ncsu.edu\cos\stat\Redirect\%username%\Documents\www /grant:r "WOLFTECH\STAT.web.service":(OI)(CI)RX /T /C /L /Q
 
@@ -30,13 +44,14 @@ set result=%datestr:/=-%
 @echo %result% > S:\Documents\.stat
 
 :PROFILE
-echo "Profile"
 if exist "s:\Documents\.statpf" GOTO :END
+echo "Profile"
 Icacls \\dc2isi00smb.oit.ncsu.edu\cos\stat\Profiles\%username%.v6\ /grant "WOLFTECH\COS-OU Admins":(OI)(CI)F /T /C /L /Q
 
 set datestr=%date%
 set result=%datestr:/=-%
 @echo %result% > S:\Documents\.statpf
+echo "...[Finishing]"
 GOTO :END
 
 :SDRIVE

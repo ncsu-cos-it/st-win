1cb030f2fc0c3d16dcc3966311f53ec90fdcfea1
tbyronnc <terry.byron@gmail.com>
2025-08-22 16:24:02 -0400

    Updated

--- bin/grant_access-avd.cmd
+++ bin/grant_access-avd.cmd
@@ -3,12 +3,15 @@ c:
 if not exist c:\temp md \Temp
 
 cd \temp
+if exist \\dc2isi00smb.oit.ncsu.edu\cos\stat\Redirect\%username% (
+	net use s: \\ncsudrive.ncsu.edu\cos\stat\Redirect\%username%
+)
 if exist s:\ GOTO :PERMS
 
 :SDRIVE
 echo "SDrive"
 md \\dc2isi00smb.oit.ncsu.edu\cos\stat\Redirect\%username%
-Icacls  \\dc2isi00smb.oit.ncsu.edu\cos\stat\Redirect\%username% /grant:r "WOLFTECH\COS-OU Admins":F /T /C /Q
+Icacls  \\dc2isi00smb.oit.ncsu.edu\cos\stat\Redirect\%username% /grant:r "WOLFTECH\COS-Share-ISI-Admins":F /T /C /Q
 Icacls  \\dc2isi00smb.oit.ncsu.edu\cos\stat\Redirect\%username% /grant:r "WOLFTECH\%USERNAME%":(NP)F /T /C /Q
 md \\dc2isi00smb.oit.ncsu.edu\cos\stat\Redirect\%username%\AppData
 md \\dc2isi00smb.oit.ncsu.edu\cos\stat\Redirect\%username%\Contacts
